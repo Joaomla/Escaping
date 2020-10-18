d3c8566fc908dbb59c966fafbd9cde188b7b4fd8
Joaomla <33231783+Joaomla@users.noreply.github.com>
2020-10-18 01:44:40 +0100

    Environment  behaviour added
    Rotation of a cube. can be used for the environment.
    Don't ask why I did on this phase of the project.

---
+++ Assets/Scripts/Environment/Levitation3D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitation3D : MonoBehaviour
+{
+    [SerializeField] float minRotationDelta = 3f;
+    [SerializeField] float maxRotationDelta = 3f;
+    private int fixedXRotation;
+    [SerializeField] float ChangeDirectionTimer = 2f;
+    private float timeLeft;
+    [SerializeField] bool changeDirAbruptly = true;
+
+    float rotationX;
+    float rotationY;
+    float rotationZ;
+
+    private void Start()
+    {
+        // Rotation along x axis will be fixed (the object looks to be either coming or going)
+        fixedXRotation = (int)Mathf.Sign(Random.Range(-1f, 1f));
+        timeLeft = ChangeDirectionTimer;
+        rotationX = Random.Range(0f, maxRotationDelta);
+        rotationY = Random.Range(-minRotationDelta, maxRotationDelta);
+        rotationZ = Random.Range(-minRotationDelta, maxRotationDelta);
+    }
+
+    void Update()
+    {
+        if (changeDirAbruptly)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                //rotationX = Random.Range(0f, maxRotationDelta);
+                rotationY = Random.Range(-minRotationDelta, maxRotationDelta);
+                rotationZ = Random.Range(-minRotationDelta, maxRotationDelta);
+                timeLeft = 2f;
+            }
+        }
+
+        transform.Rotate(fixedXRotation * rotationX, rotationY, rotationZ);
+    }
+}
