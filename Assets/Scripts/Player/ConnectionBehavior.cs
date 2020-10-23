baa02bf50573ba9e80bfca7819d3d086cef2ce8b
joaossimoes <34171469+joaossimoes@users.noreply.github.com>
2020-10-23 01:48:56 +0100

    Small Update On line
    Line is now following the objects
    Created Script to work on companion

---
+++ Assets/Scripts/Player/ConnectionBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionBehavior : MonoBehaviour
+{
+    
+    [SerializeField] GameObject companion;
+    [SerializeField] GameObject player;
+    LineRenderer lineRenderer;
+
+    Vector3 companionPos;
+    Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = this.GetComponent(typeof(LineRenderer)) as LineRenderer;
+        companionPos = companion.transform.position;
+        playerPos = player.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPositions();
+        lineRenderer.SetPosition(0, new Vector3(playerPos.x, playerPos.y, playerPos.z));
+        lineRenderer.SetPosition(1, new Vector3(companionPos.x, companionPos.y, companionPos.z));
+    }
+
+
+    public void GetPositions()
+    {
+        companionPos = companion.transform.position;
+        playerPos = player.transform.position;
+    }
+}
