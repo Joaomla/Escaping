c7280aeea23b8656598803739f200100d98f6332
Joaomla <33231783+Joaomla@users.noreply.github.com>
2021-03-14 00:09:21 +0000

    Player Attack Cooldown

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -7,6 +7,10 @@ public class Attack : MonoBehaviour
     Player player;
     AttackRange attackRange;
 
+    [Header("Attack Properties")]
+    [SerializeField] float cooldown = 2f;
+    bool canAttack = true;
+
     private void Awake()
     {
         player = GetComponent<Player>();
@@ -18,11 +22,28 @@ public class Attack : MonoBehaviour
         // Change input
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            // cooldown is not finished. can't attack
+            if(!canAttack)
+            {
+                return;
+            }
+
+            // for each enemy within range, attack
             foreach (var enemy in attackRange.enemiesWithinRange)
             {
                 // Change value
                 enemy.GetsHurt(player.AttackValue);
             }
+
+            canAttack = false;
+            StartCoroutine(AttackCooldown());
         }
     }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canAttack = true;
+    }
+
 }
