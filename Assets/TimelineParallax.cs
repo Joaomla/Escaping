c63cf2239dc026a0465ee405177f5584aef0fcd9
Joaomla <33231783+Joaomla@users.noreply.github.com>
2021-05-27 02:18:23 +0100

    added bird flying far away event

---
+++ Assets/TimelineParallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineParallax : MonoBehaviour
+{
+    public Camera camera;
+    public Transform agent;
+    public PlayableDirector timeline;
+
+    Vector2 startPosition;
+
+    Vector2 travel => (Vector2)camera.transform.position - startPosition;
+
+    float distanceFromAgent => transform.position.z - agent.position.z;
+
+    float clippingPlane => (camera.transform.position.z + (distanceFromAgent > 0 ? camera.farClipPlane : camera.nearClipPlane));
+
+    float parallaxFactor => Mathf.Abs(distanceFromAgent) / clippingPlane;
+
+    private void Update()
+    {
+        startPosition = timeline.transform.position;
+        Vector2 newPos = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, timeline.transform.position.y, timeline.transform.position.z);
+    }
+}
---
+++ Assets/TimelineUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineUtils : MonoBehaviour
+{
+    private PlayableDirector timeline;
+
+    private void Awake()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    public void StopOnFinish()
+    {
+        timeline.Stop();
+    }
+
+    public void DeactivateOnFinish()
+    {
+        gameObject.SetActive(false);
+    }
+}
