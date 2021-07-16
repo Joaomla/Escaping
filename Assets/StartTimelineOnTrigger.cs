faf825d964cf1c185fd10747aeba4c012ee5025c
Joaomla <33231783+Joaomla@users.noreply.github.com>
2021-07-16 02:05:56 +0100

    Little unimportant changes

--- Assets/StartTimelineOnTrigger.cs
+++ Assets/StartTimelineOnTrigger.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
+/// <summary>
+/// Starts timeline when triggered.
+/// Can be deactivated so that it's not retriggered.
+/// </summary>
+
 public class StartTimelineOnTrigger : MonoBehaviour
 {
-    public PlayableDirector timeline;
+    public PlayableDirector timeline; // Timeline that is activated
+    [SerializeField] bool deactivateTrigger = true; // flag that doesn't let the trigger reactivate
+    [SerializeField] string agentTag = "Player"; // tag of the object that should trigger the timeline
 
     private void OnTriggerEnter2D( Collider2D collision )
     {
-        if (collision.tag == "Player")
+        if (collision.tag == agentTag)
+        {
             timeline.Play();
+
+            if (deactivateTrigger)
+                gameObject.GetComponent<Collider2D>().enabled = false;
+        }
     }
 }
--- Assets/TimelineParallax.cs
+++ Assets/TimelineParallax.cs
@@ -7,6 +7,43 @@ public class TimelineParallax : MonoBehaviour
 {
     public Camera camera;
     public Transform agent;
+    public PlayableDirector director;
+    public float extraVelocity = 2f;
+
+    Vector2 startPosition;
+    float startZ;
+    float startY;
+    Vector3 velocity;
+    Vector3 currentDirectorPosition;
+
+    Vector2 travel => (Vector2)camera.transform.position - startPosition;
+
+    float distanceFromAgent => transform.position.z - agent.position.z;
+
+    float clippingPlane => (camera.transform.position.z + (distanceFromAgent > 0 ? camera.farClipPlane : camera.nearClipPlane));
+
+    float parallaxFactor => Mathf.Abs(distanceFromAgent) / clippingPlane;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = agent.position;
+        currentDirectorPosition = director.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 velocity = currentDirectorPosition - director.transform.position;
+        currentDirectorPosition = director.transform.position;
+        Vector2 currentDirectorPosition2D = currentDirectorPosition;
+        Vector2 newPos = currentDirectorPosition2D + velocity * extraVelocity + travel * parallaxFactor;
+        transform.position = new Vector2(newPos.x, currentDirectorPosition.y);
+    }
+
+    /*
+    public Camera camera;
+    public Transform agent;
     public PlayableDirector timeline;
 
     Vector2 startPosition;
@@ -25,4 +62,5 @@ public class TimelineParallax : MonoBehaviour
         Vector2 newPos = startPosition + travel * parallaxFactor;
         transform.position = new Vector3(newPos.x, timeline.transform.position.y, timeline.transform.position.z);
     }
+    */
 }
